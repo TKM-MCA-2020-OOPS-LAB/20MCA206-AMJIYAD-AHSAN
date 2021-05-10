142d3f0548db41a9b4f989b71e1982c99128eb28
Amjiyad <amjiyad.19@gmail.com>
2021-05-10 14:32:20 +0530

    method overloading

---
+++ CO3/methodOverloading.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class methodOverloading {
+	void area(double r)
+	{
+		System.out.println("Area of circle is:"+3.14*r*r);
+	}
+    void area(int l, int b)
+	{
+		System.out.println("Area of rectangle is:"+l*b);
+	}
+	void area(int a )
+	{
+		System.out.println("Area of square is:"+a*a);
+	}
+	
+
+	public static void main(String[] args) {
+		methodOverloading obj=new methodOverloading();
+		System.out.println("Enter the radius of circle:");
+		Scanner s=new Scanner(System.in);
+		double r=s.nextDouble();
+		obj.area(r);
+		
+		System.out.println("Enter the length and breadth of rectangle:");
+		int l=s.nextInt();
+		int b=s.nextInt();
+		obj.area(l,b);
+		
+		System.out.println("Enter the side of square:");
+		int a=s.nextInt();
+		obj.area(a);
+		
+
+		
+
+	}
+
+}
