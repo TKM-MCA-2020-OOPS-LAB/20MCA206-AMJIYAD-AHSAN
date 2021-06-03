77debb7ea4d0ff74a53a84c49d228226ea3b3c2f
Amjiyad <amjiyad.19@gmail.com>
2021-06-03 22:06:52 +0530

    package

---
+++ CO4/areas.java
@@ -0,0 +1,17 @@
+import graphics.shapes1;
+public class areas {
+	public static void main(String args[])
+	{
+	    shapes1 obj=new shapes1();
+	    System.out.println("Area of different Shapes"+"\n"+"-------------------------------");
+	    System.out.println("Circle"+"\n"+"-------------");
+	    obj.carea();
+	    System.out.println("Rectangle"+"\n"+"-------------");
+	    obj.rarea();
+	    System.out.println("Triangle"+"\n"+"--------------");
+	    obj.tarea();
+	    System.out.println("Square"+"\n"+"------------");
+	    obj.sarea();
+	}
+
+}
