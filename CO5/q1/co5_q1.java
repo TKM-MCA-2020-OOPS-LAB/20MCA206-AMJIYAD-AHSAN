d13e8e14963cb6c5359585caefe7bab6d7459bdd
Amjiyad <amjiyad.19@gmail.com>
2021-09-05 22:56:58 +0530

    shapes

---
+++ CO5/q1/co5_q1.java
@@ -0,0 +1,12 @@
+  
+import java.awt.Color;
+import java.applet.*;
+import java.awt.Graphics;
+/* <applet code = "co5_q1.class" width = "500" height = "700"></applet>*/
+public class co5_q1 extends Applet{
+public void paint(Graphics g){	
+g.drawLine(30,30, 300, 30);
+g.drawRect(120,100,270,150);
+g.drawOval(300,300,300,300);
+}
+}
