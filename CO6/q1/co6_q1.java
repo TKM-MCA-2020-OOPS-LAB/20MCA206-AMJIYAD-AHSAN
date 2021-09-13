d73c8a3423aea0a5232fc2f690f106bad823a018
Amjiyad <amjiyad.19@gmail.com>
2021-09-13 13:29:04 +0530

    modified

--- CO6/q1/co6_q1.java
+++ CO6/q1/co6_q1.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.util.*;
 public class co6_q1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
         String[] pathnames;
 
         // Creates a new File instance by converting the given pathname string
@@ -17,6 +19,22 @@ public class co6_q1 {
             // Print the names of files and directories
             System.out.println(pathname);
         }
+        String[] fileList = f.list();
+        int flag=0;
+		for(int i=0; i < fileList.length; i++)
+		{
+			String filename  = fileList[i];
+			if(filename.equalsIgnoreCase("HBase.pptx"))
+			{
+				System.out.println("\n\n" +filename + " found");
+                flag=1;
+			}
+		}
+		if(flag == 0) 
+		{
+            System.out.println("\n\nFile Not Found");
+        }
+        
     }
 
 
